7132fb0ce95d86f62ea73666ec93f8c5eb40247f
Tin Ching Lok (Twinkle) <chingtwink@gmail.com>
2021-06-14 00:02:53 -0700

    Add podfile and InputView

---
+++ RadioApp/Messager/InputView.swift
@@ -0,0 +1,11 @@
+//
+//  InputView.swift
+//  RadioApp
+//
+//  Created by Lok Tin Ching on 6/13/21.
+//
+
+import Foundation
+import SwiftUI
+
+
---
+++ RadioApp/Messager/Protocols/InputView.swift
@@ -0,0 +1,22 @@
+//
+//  InputView.swift
+//  RadioApp
+//
+//  Created by Lok Tin Ching on 6/13/21.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
